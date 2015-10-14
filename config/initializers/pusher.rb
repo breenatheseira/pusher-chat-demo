67af888a63c4a1f931cdb67bc196e0fb11865f4e
Breena Theseira <breenatheseira@yahoo.com>
2015-10-14 20:00:25 +0800

    Added: Pusher url for chat to work across multitabs

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,5 +1,7 @@
 require 'pusher'
 
+Pusher.url = "https://#{ENV['PUSHER_KEY']}:#{ENV['PUSHER_SECRET']}@api.pusherapp.com/apps/#{ENV['PUSHER_APP_ID']}"
+
 Pusher.app_id = ENV['PUSHER_APP_ID']
 Pusher.key    = ENV['PUSHER_KEY']
 Pusher.secret = ENV['PUSHER_SECRET']
