2632822950a14f5a88243c9afe7b291cde6df38c
Breena Theseira <breenatheseira@yahoo.com>
2015-10-14 19:54:36 +0800

    Added Pusher trigger

--- app/controllers/chat_messages_controller.rb
+++ app/controllers/chat_messages_controller.rb
@@ -1,10 +1,22 @@
 class ChatMessagesController < ApplicationController
   def index
   	@chat_message = ChatMessage.new
+
+  	# Pusher.get('chat','new_message')
+
   end
 
   def create
   	@chat_message = ChatMessage.new(params[:chat_message])
-  	respond_to { |format| format.js }
+  	logger.info "app id: #{ENV['PUSHER_APP_ID']}"
+  	logger.info "key: #{ENV['PUSHER_KEY']}"
+  	logger.info "secret: #{ENV['PUSHER_SECRET']}"
+
+  	Pusher.trigger('chat','new_message', {
+  		name: @chat_message.name,
+  		message: @chat_message.message
+  		})
+
+  	respond_to :js
   end
 end
